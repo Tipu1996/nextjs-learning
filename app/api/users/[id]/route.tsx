5ad8ba72e440c1f3b679440a83cf302570633619
Abdullah Ashfaq <tipu96@Abdullahs-Air.lan>
2024-05-04 03:10:00 +0300

    created hardcoded api endpoint delete

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,13 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 
 interface Props {
-	params: { id: number; name: string };
+	params: { id: number };
 }
 
-export function get({ params: { id } }: Props) {
+export function GET({ params: { id } }: Props) {
 	if (id > 10)
 		return NextResponse.json({ error: "user not found" }, { status: 404 });
 	else return NextResponse.json({ id: 1, name: "Tipu" });
 }
 
-export function PUT(request: NextRequest, { params: { id, name } }: Props) {}
+export async function PUT(request: NextRequest, { params: { id } }: Props) {
+	const body = await request.json();
+	if (!body.name)
+		return NextResponse.json(
+			{ error: "Name is required" },
+			{ status: 400 },
+		);
+	else if (id > 10)
+		return NextResponse.json({ error: "user not found" }, { status: 404 });
+	else return NextResponse.json({ id: 1, name: "Sultan" });
+}
+
+export async function DELETE(request: NextRequest, { params: { id } }: Props) {
+	if (id > 10)
+		return NextResponse.json({ error: "user not found" }, { status: 404 });
+	else return NextResponse.json({});
+}
