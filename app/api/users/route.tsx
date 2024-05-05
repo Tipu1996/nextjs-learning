18120c1c78b699273d6380657a07a3bbd5fc104c
Abdullah Ashfaq <tipu96@Abdullahs-Air.lan>
2024-05-05 04:54:11 +0300

    added CRUD functionality to users with MySQL using prisma

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,17 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "../../../prisma/client";
 
-export function GET() {
-	return NextResponse.json([
-		{ id: 1, name: "Tipu" },
-		{ id: 2, name: "Sultan" },
-	]);
+export async function GET() {
+	const users = await prisma.user.findMany();
+	return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
 	const body = await request.json();
 	const validation = schema.safeParse(body);
+	const user = await prisma.user.findUnique({ where: { email: body.email } });
 	if (!validation.success)
 		return NextResponse.json(validation.error.errors, { status: 400 });
-	else return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+	if (user)
+		return NextResponse.json(
+			{ error: "user already exists" },
+			{ status: 400 },
+		);
+	else {
+		const newUser = await prisma.user.create({
+			data: { name: body.name, email: body.email },
+		});
+		return NextResponse.json(newUser, { status: 201 });
+	}
 }
